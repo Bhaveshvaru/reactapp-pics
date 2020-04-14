4a5bfa0032a3aad21ea91e3144ea03542d72aa4e
BhaveshVaru <bhavehs.varu97@gmail.com>
2020-04-14 15:19:18 +0530

    Updates

---
+++ precache-manifest.e0ae6aa50b0581f8480b5d86deae6333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dc344126ea7618c0d4c2445352cf11",
+    "url": "/reactapp-pics/index.html"
+  },
+  {
+    "revision": "4a0859b8cb264be3314e",
+    "url": "/reactapp-pics/static/css/main.e730424a.chunk.css"
+  },
+  {
+    "revision": "2d4bd92103f9d0f3bcaf",
+    "url": "/reactapp-pics/static/js/2.c4df6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp-pics/static/js/2.c4df6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0859b8cb264be3314e",
+    "url": "/reactapp-pics/static/js/main.7343e9c1.chunk.js"
+  },
+  {
+    "revision": "e21dad45952b2a8482cf",
+    "url": "/reactapp-pics/static/js/runtime-main.3a647118.js"
+  }
+]);
